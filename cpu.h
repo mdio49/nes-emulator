88b4b61325ed20f44506a42026333a2df7dd8682
mdio49 <marco9.dio@gmail.com>
2022-03-25 11:40:59 +1100

    Replaced some tabs with spaces.

--- cpu.h
+++ cpu.h
@@ -45,17 +45,17 @@ typedef uint16_t addr_t;
  */
 typedef struct addrmode {
 
-	/**
-	 * Gets the absolute address of the argument using this address mode.
-	 */
-	addr_t (*get_address)(tframe_t *frame, uint8_t *mem, uint8_t *args);
+    /**
+    * Gets the absolute address of the argument using this address mode.
+    */
+    addr_t (*get_address)(tframe_t *frame, uint8_t *mem, uint8_t *args);
 
-	/**
-	 * The number of operator arguments (bytes) required by this address mode
-	 * in order to obtain the address. Upon execution of an instruction, the
-	 * program counter is incremented by 1 + this value.
-	 */
-	uint8_t argc;
+    /**
+    * The number of operator arguments (bytes) required by this address mode
+    * in order to obtain the address. Upon execution of an instruction, the
+    * program counter is incremented by 1 + this value.
+    */
+    uint8_t argc;
 
 } addrmode_t;
 
@@ -63,31 +63,31 @@ typedef struct addrmode {
  * There are a wide range of addresisng modes, and not all instructions use every one.
  * 
  * IMPLIED:                 Argument is implied (i.e. instruction takes no arguments).
- * ACCUMULATOR:	A		    Argument is value from accumulator (instruction takes no arguments).
- * IMMEDIATE:	#$BB		Instruction uses the constant value provided. If an instruction uses this address mode,
+ * ACCUMULATOR: A           Argument is value from accumulator (instruction takes no arguments).
+ * IMMEDIATE:   #$BB        Instruction uses the constant value provided. If an instruction uses this address mode,
  *                          then the pointer to the address mode type is NULL.
  * 
- * ZEROPAGE:	$LL			Zero-page addressing. Instruction applies to the memory location $00LL.
- * ZEROPAGEX:   $LL,X		Same as zero page, but offset by the value at register X. Wraps around if larger than 255
- *						    so that memory address is always between $0000 and $00FF. Does not use the carry bit.
- * ZEROPAGEY:   $LL,Y		Same as zero page X, but for the register Y.
+ * ZEROPAGE:    $LL         Zero-page addressing. Instruction applies to the memory location $00LL.
+ * ZEROPAGEX:   $LL,X       Same as zero page, but offset by the value at register X. Wraps around if larger than 255
+ *                          so that memory address is always between $0000 and $00FF. Does not use the carry bit.
+ * ZEROPAGEY:   $LL,Y       Same as zero page X, but for the register Y.
  * 
- * ABSOLUTE:	$LLHH		Absolute addressing. Instruction appllies to the memory location $HHLL.
- * ABSOLUTEX:	$LLHH,X.	Same as absolute, but offset by the value at register X. Uses the carry bit.
- * ABSOLUTEY:	$LLHH,Y.	Same as absolute X, but for the register Y.
+ * ABSOLUTE:    $LLHH       Absolute addressing. Instruction appllies to the memory location $HHLL.
+ * ABSOLUTEX:   $LLHH,X.    Same as absolute, but offset by the value at register X. Uses the carry bit.
+ * ABSOLUTEY:   $LLHH,Y.    Same as absolute X, but for the register Y.
  * 
  * RELATIVE:    $BB         Relative addressing. Target address is program counter offset by the signed value $BB.
  * 
- * INDIRECT:	($LLHH)		Indirect addressing. Similar to a pointer. Instruction takes the value at memory addresses
- *						    $HHLL and $HHLL+1 and uses that to form a new memory address. The value at the first address
- *						    gives the least significant byte for the new address and the value at the second address
- *						    gives the most significant byte, so the new address is ($HHLL+1, $HHLL).
- * INDIRECTX:	($LL,X)		Indexed-indirect addressing. Takes the zero-page address $00LL, increments it by the value
- *						    at register X wrapped between 0-255, then use it to lookup a 2-byte memory address.
- *						    ~ Basically a composition of zeropage-x and indirect.
- * INDIRECTY:	($LL),Y     Indirect-indexed addressing. Take the zero-page address $LL, dereference it to form a 2-byte
- *						    memory address, then add the value at register Y to form the final memory address.
- *						    ~ Basically a composition of zeropage, indirect and y-indexed absolute.
+ * INDIRECT:    ($LLHH)     Indirect addressing. Similar to a pointer. Instruction takes the value at memory addresses
+ *                          $HHLL and $HHLL+1 and uses that to form a new memory address. The value at the first address
+ *                          gives the least significant byte for the new address and the value at the second address
+ *                          gives the most significant byte, so the new address is ($HHLL+1, $HHLL).
+ * INDIRECTX:   ($LL,X)     Indexed-indirect addressing. Takes the zero-page address $00LL, increments it by the value
+ *                          at register X wrapped between 0-255, then use it to lookup a 2-byte memory address.
+ *                          ~ Basically a composition of zeropage-x and indirect.
+ * INDIRECTY:   ($LL),Y     Indirect-indexed addressing. Take the zero-page address $LL, dereference it to form a 2-byte
+ *                          memory address, then add the value at register Y to form the final memory address.
+ *                          ~ Basically a composition of zeropage, indirect and y-indexed absolute.
  */
 
 extern const addrmode_t AM_IMPLIED;
